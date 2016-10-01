df37f5c2c07ed4e539686e6114bfcd7883391bcf
David Feldbaum <Dfeldbaum@Davids-MacBook-Pro-2.local>
2016-09-30 23:09:22 -0500

    updated through 3

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -1,12 +1,10 @@
 console.log("Linked.");
 
-// Dramatis Personae
-var hobbits = [
-  'Frodo Baggins',
-  'Samwise \'Sam\' Gamgee',
-  'Meriadoc \'Merry\' Brandybuck',
-  'Peregrin \'Pippin\' Took'
-];
+
+// var article = $('<article>');
+
+
+// GLOBAL VARIABLES !!!!
 
 var buddies = [
   'Gandalf the Grey',
@@ -16,24 +14,94 @@ var buddies = [
   'Boromir'
 ];
 
-var lands = ['The Shire', 'Rivendell', 'Mordor'];
-var body = document.body;
-var section = '<section></section>';
+var section = $('<section>');
 
-function makeMiddleEarth() {
-  // your answers here
-}
+var body = $('body');
+
+
+// Part 1
+// create a section tag with an id of `middle-earth`
+// add each land as an `article` tag
+// inside each `article` tag include an `h1` with the name of the land
+// append `middle-earth` to your document `body`
+
+
+
+function makeMiddleEarth () {
+
+  var lands = ['The Shire', 'Rivendell', 'Mordor'];
+  
+
+  body.append(section)   
+
+  section.prop('id', 'middle-earth');
+
+  $.each(lands, function(index, value) {
+    $('#middle-earth').append('<article>' + '<h1>' + value + '</h1>' + '</article>');
+  });
+
+  } 
+
+
 
 makeMiddleEarth();
 
-function makeHobbits(){
-  // your answers here
+
+// Part 2
+
+// display an `unordered list` of hobbits in the shire
+// (which is the second article tag on the page)
+// give each hobbit a class of `hobbit`
+
+function makeHobbits () {
+
+
+  var hobbitShire = $('#middle-earth').children('article')[0]; // selecting first 
+
+
+  var hobbits = [
+    'Frodo Baggins',
+    'Samwise \'Sam\' Gamgee',
+    'Meriadoc \'Merry\' Brandybuck',
+    'Peregrin \'Pippin\' Took'];  
+   
+  var list = $('<ul>');
+  var hobbit = $(hobbitShire).append(list);
+
+  $.each(hobbits, function(index, value) {
+    $(list).append('<li class="hobbit">' + value + '</li>');
+
+    });
+
+
+
 }
+
 
 makeHobbits();
 
+
+// Part 3 
+
 function keepItSecretKeepItSafe(){
-  // your answers here
+  
+// create a div with an id of `'the-ring'`
+// give the div a class of `'magic-imbued-jewelry'`
+// add the ring as a child of `Frodo`
+  
+  
+
+  // var frodo = 
+
+  var divRing = $('<div>').prop('id', 'the-ring').prop('class', 'magic-imbued-jewelry');
+  // frodo.append(divRing)
+
+
+
+
+
+
+
 }
 
 keepItSecretKeepItSafe();
